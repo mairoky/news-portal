14cc7faf2dc64bc82054b236a5316147d983a687
Aminul Islam Rocky <mairoky@outlook.com>
2022-09-03 21:41:26 +0600

    responsive

--- js/script.js
+++ js/script.js
@@ -63,11 +63,11 @@ const displayNews = news => {
         newsDiv.classList.add('col-12');
         newsDiv.innerHTML = `
         <div onclick="newsDetails('${singleNews._id}')" class="card border-0 p-3" data-bs-toggle="modal" data-bs-target="#newsModal">
-        <div class="row g-0 align-items-center">
-            <div class="col-3">
-                <img src="${singleNews.thumbnail_url}" class="img-fluid">
+        <div class="row g-0 align-items-lg-center">
+            <div class="col-md-3 d-block text-center">
+                <img src="${singleNews.thumbnail_url}" class="img-fluid h-100">
             </div>
-            <div class="col-9">
+            <div class="col-md-9 d-block">
                 <div class="card-body">
                     <h4 class="card-title">${singleNews.title}</h4>
                     <p class="card-text">${singleNews.details.slice(0, 200)}.</p>
@@ -78,23 +78,23 @@ const displayNews = news => {
                                 <img src="${singleNews.author.img}" alt="" class="img-fluid rounded-circle">
                             </div>
                             <div class="info ms-3">
-                                <p class="m-0">${singleNews.author.name ? singleNews.author.name : 'No Data Found!'}</p>
-                                <p class="m-0"><small>${singleNews.author.published_date ? singleNews.author.published_date : 'No Data Found!'}</small></p>
+                                <p class="m-0"><small>${singleNews.author.name ? singleNews.author.name : 'Not Found!'}</small></p>
+                                <p class="m-0"><small>${singleNews.author.published_date ? singleNews.author.published_date : 'Not Found!'}</small></p>
                             </div>
                         </div>
                         <div class="news-views p-2">
                             <i class="fa-regular fa-eye"></i>
-                            <span class="ms-2">${singleNews.total_view ? singleNews.total_view : 'No data found!'}</span>
+                            <span class="ms-2"><small>${singleNews.total_view ? singleNews.total_view : 'Not Found!'}</small></span>
                         </div>
-                        <div class="rating p-2">
+                        <div class="rating p-2 d-none d-sm-block"><small>
                             <i class="fa-solid fa-star"></i>
                             <i class="fa-solid fa-star-half-stroke"></i>
                             <i class="fa-regular fa-star"></i>
                             <i class="fa-regular fa-star"></i>
-                            <i class="fa-regular fa-star"></i>
+                            <i class="fa-regular fa-star"></i></small>
                         </div>
-                        <div class="read-more-icon p-2">
-                            <i class="fa-solid fa-arrow-right"></i>
+                        <div class="read-more-icon p-2 d-none d-sm-block"><small>
+                            <i class="fa-solid fa-arrow-right"></i></small>
                         </div>
                     </div>
                 </div>
@@ -149,20 +149,20 @@ const showNewsDetails = (newsDetail) => {
     newsTitle.innerText = `${newsDetail.title}`;
     const newsDetailModal = document.getElementById('news-detail');
     newsDetailModal.innerHTML = `
-        <h5 class="">Rating: ${newsDetail.rating.number ? newsDetail.rating.number : 'No data found!'}, Badge: ${newsDetail.rating.badge ? newsDetail.rating.badge : 'No data found!'}</h5>
+        <h5 class="rating-text">Rating: ${newsDetail.rating.number ? newsDetail.rating.number : 'Not Found!'}, Badge: ${newsDetail.rating.badge ? newsDetail.rating.badge : 'Not Found!'}</h5>
         <div class="d-flex justify-content-between align-items-center">
             <div class="author-info p-2 d-flex align-items-center">
                 <div class="image">
                     <img src="${newsDetail.author.img}" alt="" class="img-fluid rounded-circle">
                 </div>
                 <div class="info ms-3">
-                    <p class="m-0">${newsDetail.author.name ? newsDetail.author.name : 'No Data Found!'}</p>
-                    <p class="m-0"><small>${newsDetail.author.published_date ? newsDetail.author.published_date : 'No Data Found!'}</small></p>
+                    <p class="m-0"><small>${newsDetail.author.name ? newsDetail.author.name : 'Not Found!'}</small></p>
+                    <p class="m-0"><small>${newsDetail.author.published_date ? newsDetail.author.published_date : 'Not Found!'}</small></p>
                 </div>
             </div>
             <div class="news-views p-2">
                 <i class="fa-regular fa-eye"></i>
-                    <span class="ms-2">${newsDetail.total_view ? newsDetail.total_view : 'No data found!'}</span>
+                    <span class="ms-2"><small>${newsDetail.total_view ? newsDetail.total_view : 'Not Found!'}</small></span>
             </div>
         </div>
         <div class="mt-2">
